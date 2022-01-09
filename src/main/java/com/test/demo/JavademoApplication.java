51a83b16b3fbede101a906fc330df472379e2c9c
SanjeevKumarSrivastava <sksanjeev4455@gmail.com>
2022-01-09 21:24:43 +0530

    Created the controller class/Updated the server port

--- src/main/java/com/test/demo/JavademoApplication.java
+++ src/main/java/com/test/demo/JavademoApplication.java
@@ -3,11 +3,16 @@ package com.test.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.test.demo.controller.TestController;
+
 @SpringBootApplication
 public class JavademoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavademoApplication.class, args);
+		
+		TestController testController = new TestController();
+		testController.mtTestMethod();
 	}
 
 }
